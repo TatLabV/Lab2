e251173e80f7576814f31ee76fc4e535e8de8bf4
TatLabV <zinkaloid2@gmail.com>
2025-09-27 09:32:01 +0300

    3task.py

--- 3task.py
+++ 3task.py
@@ -1,6 +1,6 @@
-import math_operations as mo  # Импортируем модуль под коротким именем
+import math_operations as mo  #Импортируем модуль
 
-# Примеры использования функций
+#Примеры использования функций
 a = 10
 b = 3
 
@@ -12,3 +12,4 @@ try:
     print(f"{a} / {b} = {mo.divide(a, b)}")
 except ValueError:
     print("Ошибка: деление на ноль!")
+
