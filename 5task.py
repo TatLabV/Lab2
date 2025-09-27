4b5be6848329fed58f5af79c135a60a1f30a79d5
TatLabV <zinkaloid2@gmail.com>
2025-09-27 09:32:40 +0300

    5task.py

--- 5task.py
+++ 5task.py
@@ -1,13 +1,14 @@
-# Создаем список чисел от 1 до 20
+#Создаем список чисел от 1 до 20
 numbers = list(range(1, 21))
 
-# 1. Фильтруем четные числа
+#Фильтруем четные числа
 filtered_even = list(filter(lambda x: x % 2 == 0, numbers))
 
-# 2. Увеличиваем каждое число на 10
+#Увеличиваем каждое число на 10
 incremented = list(map(lambda x: x + 10, filtered_even))
 
-# 3. Сортируем по убыванию
+#Сортируем по убыванию
 result = sorted(incremented, reverse=True)
 
 print(result)
+
